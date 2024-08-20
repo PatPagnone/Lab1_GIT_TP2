e3ef9b6bd79c05fc5a0f2da37a7d6dd46ef09044
PatPagnone <muralismo.sanluis@gmail.com>
2024-08-20 19:55:49 -0300

    hola
    se agregó la clase OtraClase

--- src/lab1_git_tp2/Lab1_GIT_TP2.java
+++ src/lab1_git_tp2/Lab1_GIT_TP2.java
@@ -9,7 +9,8 @@ public class Lab1_GIT_TP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ejercicio 2");
+        System.out.println("Agregar cambios.");
     }
 
 }
